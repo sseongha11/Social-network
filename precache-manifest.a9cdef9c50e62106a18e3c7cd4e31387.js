d39f2814dd250b0497bd9499b1a6d15987f18f8b
Seongha <sseongha11@gmail.com>
2020-03-04 22:41:33 +0000

    Updates

---
+++ precache-manifest.a9cdef9c50e62106a18e3c7cd4e31387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "622d3b64150f7abe91f9",
+    "url": "/social-network/static/css/main.e8c3227c.chunk.css"
+  },
+  {
+    "revision": "622d3b64150f7abe91f9",
+    "url": "/social-network/static/js/main.622d3b64.chunk.js"
+  },
+  {
+    "revision": "ca0762182e564900f728",
+    "url": "/social-network/static/css/1.8fb7c46e.chunk.css"
+  },
+  {
+    "revision": "ca0762182e564900f728",
+    "url": "/social-network/static/js/1.ca076218.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "927b9b893827ab21d48c1a69fde1cdef",
+    "url": "/social-network/index.html"
+  }
+];
